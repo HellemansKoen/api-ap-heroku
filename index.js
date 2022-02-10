232a6138e9d1fd07a8fe37cac31ffa4b5d7a148e
HellemansKoen <78733780+HellemansKoen@users.noreply.github.com>
2022-02-10 22:15:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: *
-        /*origin: 'https://xenodochial-gates-cf6795.netlify.app/' */
+        origin: 'https://xenodochial-gates-cf6795.netlify.app/'
     })
 );
 app.options('*', cors());
 
-app.get('/', (req, res) => res.send('I modified this API'));
+app.get('/', (req, res) => res.send('I modified this API.'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
